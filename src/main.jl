ab669d66be70edbb9175e572d9ab4df97d525de5
Anil Yildiz <yildiz@stanford.edu>
2022-03-26 23:46:13 -0700

    belief and simulator methods for MODIA_of_POMDPs

--- src/main.jl
+++ src/main.jl
@@ -1,8 +1,14 @@
 using POMDPs
 using BeliefUpdaters
 using POMDPPolicies: action
+using Random
+using POMDPSimulators
 
 abstract type MODIA end
+struct MODIA_Policies; policies; end
+struct MODIA_Updater; updater; end
+struct MODIA_Beliefs; beliefs; end
+struct MODIA_Distribution; dist; end
 
 mutable struct MODIA_of_POMDPs <: MODIA
     DPs
@@ -27,6 +33,7 @@ mutable struct MODIA_of_Mixture <: MODIA
     beliefs
 end
 
+
 function MODIA(DPs, DCs, SSF)
     beliefs = nothing
     dps = [[item] for item in DPs]
@@ -41,62 +48,129 @@ function MODIA(DPs, DCs, SSF)
     return modia
 end
 
-function MODIA(DPs, DCs, SSF, beliefs::Function)
+function MODIA(DPs, DCs, SSF, belief_func::Function)
     dps = [[item] for item in DPs]
     markov_prcs = repeat.(dps, DCs)
     if all(isa.(DPs, POMDP))
-        modia = MODIA_of_POMDPs(DPs, DCs, SSF, markov_prcs, beliefs)
-        initialize_beliefs!(modia, beliefs)
+        modia = MODIA_of_POMDPs(DPs, DCs, SSF, markov_prcs, nothing)
+        initialize_beliefs!(modia, belief_func)
 
     elseif all(isa.(DPs, MDP))
-        modia = MODIA_of_MDPs(DPs, DCs, SSF, markov_prcs)
+        error("Cannot use `belief_func` for MDPs. Call MODIA(DPs, DCs, SSF) instead.")
 
     else
-        modia = MODIA_of_Mixture(DPs, DCs, SSF, markov_prcs, beliefs)
-        initialize_beliefs!(modia, beliefs)   # TODO
+        modia = MODIA_of_Mixture(DPs, DCs, SSF, markov_prcs, nothing)
+        initialize_beliefs!(modia, belief_func)   # TODO
 
     end
     return modia
 end
 
-"""Collects a field of a MODIA as a 1D Vector. Defaults to the field `markov_prcs`."""
-collect(modia::MODIA, field::Symbol=:markov_prcs) = vcat(getproperty(modia, field)...)
-
-
-"""Returns a set of all possible actions among MODIA (PO)MDPs."""
-POMDPs.actions(modia::MODIA) = Set(vcat(actions.(collect(modia))...))
-
 """Initialize belief for all POMDPs in a MODIA."""
 function initialize_beliefs!(modia::MODIA_of_POMDPs, belief_func::Function)
-    beliefs = [belief_func.(sublist) for sublist in modia.markov_prcs]
+    beliefs = MODIA_Beliefs([belief_func.(sublist) for sublist in modia.markov_prcs])
     modia.beliefs = beliefs
     return
 end
 
-"""Compute optimal policies for all POMDPs in a MODIA."""
-POMDPs.solve(solver::Solver, modia::MODIA_of_POMDPs) = [solve.(Ref(solver), sublist) for sublist in modia.markov_prcs]
+"""Collects `markov_prcs` in a MODIA as a 1D Vector."""
+collect(modia::MODIA) = vcat(getproperty(modia, :markov_prcs)...)
+
+"""Collects `beliefs` in a MODIA as a 1D Vector."""
+function collect_beliefs(modia::MODIA) 
+    field = :beliefs
+    dt = getproperty(modia, field)
+    f = fieldnames(typeof(dt))[1]
+    return vcat(getproperty(dt,f)...)
+end
+
 
 """Returns the safest action among all possible actions in MODIA (PO)MDPs."""
-function safest_action(policies, modia::MODIA)
-    acts = POMDPs.action.(vcat(policies...), vcat(modia.beliefs...))
+function safest_action(policies::MODIA_Policies, modia::MODIA)
+    acts = POMDPs.action.(vcat(policies.policies...), vcat(modia.beliefs.beliefs...))
     return modia.SSF(acts)
 end
 
-BeliefUpdaters.update(bu, modia::MODIA_of_POMDPs, act, obs) = [update.(bu[idx], sublist, Ref(act), obs[idx]) for (idx, sublist) in enumerate(modia.beliefs)]
 
-function update!(bu, modia::MODIA_of_POMDPs, act, obs)
+
+### POMDPs.jl overloads ###
+
+"""Returns a set of all possible actions among MODIA (PO)MDPs."""
+POMDPs.actions(modia::MODIA) = Set(vcat(actions.(collect(modia))...))
+
+"""Compute optimal policies for all POMDPs in a MODIA."""
+POMDPs.solve(solver::Solver, modia::MODIA_of_POMDPs) = MODIA_Policies([solve.(Ref(solver), sublist) for sublist in modia.markov_prcs])
+
+POMDPs.updater(policies::MODIA_Policies) = MODIA_Updater([POMDPs.updater(pol[1]) for pol in policies.policies])
+
+POMDPs.initialstate(modia::MODIA_of_POMDPs) = MODIA_Distribution([initialstate.(sublist) for sublist in modia.markov_prcs])
+POMDPs.initialobs(modia::MODIA_of_POMDPs, s0) = MODIA_Distribution([initialobs.(sublist, s0[idx]) for (idx,sublist) in enumerate(modia.markov_prcs)])
+
+POMDPs.rand(rng::AbstractRNG, d::MODIA_Distribution) = [rand.(Ref(rng),sublist) for sublist in d.dist]
+POMDPs.rand(d::MODIA_Distribution) = [rand.(sublist) for sublist in d.dist]
+
+
+### BeliefUpdaters.jl overloads ###
+
+BeliefUpdaters.update(bu::MODIA_Updater, modia::MODIA_of_POMDPs, act, obs) = MODIA_Beliefs([update.(Ref(bu.updater[idx]), sublist, Ref(act), obs[idx]) for (idx, sublist) in enumerate(modia.beliefs.beliefs)])
+
+function update!(bu::MODIA_Updater, modia::MODIA_of_POMDPs, act, obs)
     modia.beliefs = BeliefUpdaters.update(bu, modia::MODIA_of_POMDPs, act, obs)
     return
 end
 
-BeliefUpdaters.DiscreteUpdater(modia::MODIA_of_POMDPs) = [BeliefUpdaters.DiscreteUpdater.(sublist) for sublist in modia.markov_prcs]
+BeliefUpdaters.DiscreteUpdater(modia::MODIA_of_POMDPs) = MODIA_Updater([BeliefUpdaters.DiscreteUpdater(sublist[1]) for sublist in modia.markov_prcs])
+BeliefUpdaters.NothingUpdater(modia::MODIA_of_POMDPs) = MODIA_Updater([BeliefUpdaters.NothingUpdater() for sublist in modia.markov_prcs])
+BeliefUpdaters.PreviousObservationUpdater(modia::MODIA_of_POMDPs) = MODIA_Updater([BeliefUpdaters.PreviousObservationUpdater() for sublist in modia.markov_prcs])
+BeliefUpdaters.KMarkovUpdater(modia::MODIA_of_POMDPs, k::Int) = MODIA_Updater([BeliefUpdaters.KMarkovUpdater(k) for sublist in modia.markov_prcs])
+
+BeliefUpdaters.initialize_belief(modia::MODIA_Updater, initial_obs_vec::AbstractVector) = MODIA_Beliefs([BeliefUpdaters.PreviousObservations.(eachcol(transpose(hcat(item...)))) for item in eachrow(hcat(initial_obs_vec...))])
+
+BeliefUpdaters.uniform_belief(modia::MODIA_of_POMDPs) = MODIA_Beliefs([uniform_belief.(sublist) for sublist in modia.markov_prcs])
 
 
+### POMDPSimulators.jl overloads ###
+
+POMDPSimulators.simulate(sim::RolloutSimulator, modia::MODIA, policies::MODIA_Policies) = POMDPSimulators.simulate(sim, modia, policies, POMDPs.updater(policies), modia.beliefs)
+POMDPSimulators.simulate(sim::RolloutSimulator, modia::MODIA, policies::MODIA_Policies, bu::MODIA_Updater) = POMDPSimulators.simulate(sim, modia, policies, bu, modia.beliefs)
+POMDPSimulators.simulate(sim::RolloutSimulator, modia::MODIA, policies::MODIA_Policies, bu::MODIA_Updater, initial_beliefs::MODIA_Beliefs) = [POMDPSimulators.simulate.(Ref(sim), sublist, policies.policies[idx], Ref(bu.updater[idx]), initial_beliefs.beliefs[idx]) for (idx,sublist) in enumerate(modia.markov_prcs)]
+
+POMDPSimulators.simulate(sim::RolloutSimulator, modia::MODIA, policies::MODIA_Policies, initial_states) = POMDPSimulators.simulate(sim, modia, policies, POMDPs.updater(policies), modia.beliefs, initial_states)
+POMDPSimulators.simulate(sim::RolloutSimulator, modia::MODIA, policies::MODIA_Policies, bu::MODIA_Updater, initial_states) = POMDPSimulators.simulate(sim, modia, policies, bu, modia.beliefs, initial_states)
+POMDPSimulators.simulate(sim::RolloutSimulator, modia::MODIA, policies::MODIA_Policies, bu::MODIA_Updater, initial_beliefs::MODIA_Beliefs, initial_states) = [POMDPSimulators.simulate.(Ref(sim), sublist, policies.policies[idx], Ref(bu.updater[idx]), initial_beliefs.beliefs[idx], initial_states[idx]) for (idx,sublist) in enumerate(modia.markov_prcs)]
 
 
+POMDPSimulators.simulate(sim::HistoryRecorder, modia::MODIA, policies::MODIA_Policies) = POMDPSimulators.simulate(sim, modia, policies, POMDPs.updater(policies), modia.beliefs)
+POMDPSimulators.simulate(sim::HistoryRecorder, modia::MODIA, policies::MODIA_Policies, bu::MODIA_Updater) = POMDPSimulators.simulate(sim, modia, policies, bu, modia.beliefs)
+POMDPSimulators.simulate(sim::HistoryRecorder, modia::MODIA, policies::MODIA_Policies, bu::MODIA_Updater, initial_beliefs::MODIA_Beliefs) = [POMDPSimulators.simulate.(Ref(sim), sublist, policies.policies[idx], Ref(bu.updater[idx]), initial_beliefs.beliefs[idx]) for (idx,sublist) in enumerate(modia.markov_prcs)]
 
-function stepthrough(modia::MODIA)
+POMDPSimulators.simulate(sim::HistoryRecorder, modia::MODIA, policies::MODIA_Policies, initial_states) = POMDPSimulators.simulate(sim, modia, policies, POMDPs.updater(policies), modia.beliefs, initial_states)
+POMDPSimulators.simulate(sim::HistoryRecorder, modia::MODIA, policies::MODIA_Policies, bu::MODIA_Updater, initial_states) = POMDPSimulators.simulate(sim, modia, policies, bu, modia.beliefs, initial_states)
+POMDPSimulators.simulate(sim::HistoryRecorder, modia::MODIA, policies::MODIA_Policies, bu::MODIA_Updater, initial_beliefs::MODIA_Beliefs, initial_states) = [POMDPSimulators.simulate.(Ref(sim), sublist, policies.policies[idx], Ref(bu.updater[idx]), initial_beliefs.beliefs[idx], initial_states[idx]) for (idx,sublist) in enumerate(modia.markov_prcs)]
+
+
+POMDPSimulators.simulate(sim::StepSimulator, modia::MODIA, policies::MODIA_Policies) = POMDPSimulators.simulate(sim, modia, policies, POMDPs.updater(policies), modia.beliefs)
+POMDPSimulators.simulate(sim::StepSimulator, modia::MODIA, policies::MODIA_Policies, bu::MODIA_Updater) = POMDPSimulators.simulate(sim, modia, policies, bu, modia.beliefs)
+POMDPSimulators.simulate(sim::StepSimulator, modia::MODIA, policies::MODIA_Policies, bu::MODIA_Updater, initial_beliefs::MODIA_Beliefs) = [POMDPSimulators.simulate.(Ref(sim), sublist, policies.policies[idx], Ref(bu.updater[idx]), initial_beliefs.beliefs[idx]) for (idx,sublist) in enumerate(modia.markov_prcs)]
+
+POMDPSimulators.simulate(sim::StepSimulator, modia::MODIA, policies::MODIA_Policies, initial_states) = POMDPSimulators.simulate(sim, modia, policies, POMDPs.updater(policies), modia.beliefs, initial_states)
+POMDPSimulators.simulate(sim::StepSimulator, modia::MODIA, policies::MODIA_Policies, bu::MODIA_Updater, initial_states) = POMDPSimulators.simulate(sim, modia, policies, bu, modia.beliefs, initial_states)
+POMDPSimulators.simulate(sim::StepSimulator, modia::MODIA, policies::MODIA_Policies, bu::MODIA_Updater, initial_beliefs::MODIA_Beliefs, initial_states) = [POMDPSimulators.simulate.(Ref(sim), sublist, policies.policies[idx], Ref(bu.updater[idx]), initial_beliefs.beliefs[idx], initial_states[idx]) for (idx,sublist) in enumerate(modia.markov_prcs)]
+
+
+function POMDPSimulators.stepthrough(modia::MODIA_of_POMDPs, policies::MODIA_Policies, args...; kwargs...)
+    spec_included=false
+    if !isempty(args) && isa(last(args), Union{String, Tuple, Symbol})
+        spec = last(args)
+        spec_included = true
+        # if spec isa statetype(pomdp) && length(args) == 3
+        #     error("Ambiguity between `initial_state` and `spec` arguments in stepthrough. Please explicitly specify the initial state and spec.")
+        # end
+    else
+        spec = tuple(:s, :a, :sp, :o, :r, :info, :t, :action_info, :b, :bp, :update_info)
+    end
+    sim = StepSimulator(spec; kwargs...)
+    return simulate(sim, modia, policies, args[1:end-spec_included]...)
 end
 
-function simulate(modia::MODIA)
-end
+
--- test/tiger_pomdp_test.jl
+++ test/tiger_pomdp_test.jl
@@ -1,8 +1,11 @@
 using POMDPs
 using POMDPModels: TigerPOMDP
 using QMDP: QMDPSolver
-using BeliefUpdaters: DiscreteBelief, DiscreteUpdater, uniform_belief, update
+using BeliefUpdaters
 using POMDPPolicies: action
+using Random
+using Printf
+using POMDPSimulators
 
 tiger_problem1 = TigerPOMDP(-1.0, -100.0, 10.0, 0.90, 0.90)
 tiger_problem2 = TigerPOMDP(-2.0, -50.0, 17.0, 0.80, 0.60)
@@ -20,9 +23,6 @@ initialize_beliefs!(modia, uniform_belief)
 ## Method 2 ##
 modia = MODIA(DPs, DCs, SSF, uniform_belief)
 
-# ## Method 3 ##
-# modia = MODIA(DPs, DCs, SSF)
-# modia.belief = object_here  # TODO
 
 """Inspect the `modia` object"""
 collect(modia)
@@ -37,9 +37,57 @@ act = safest_action(policies, modia)
 
 
 """Update beliefs of DCs in the MODIA"""
-bu = DiscreteUpdater(modia);
-random_obs = [[Bool(rand(0:1)) for _ in sublist] for sublist in modia.markov_prcs]  # Create random observations for all DCs
+bu = DiscreteUpdater(modia);    # can also use updater(policies) to retrieve an appropiate belief updater
+random_obs = [[Bool(rand(0:1)) for _ in sublist] for sublist in modia.markov_prcs]  # create random observations for all DCs
 new_belief = update(bu, modia, act, random_obs);   # calculates new belief, but does not update within modia
 
-update!(bu, modia, act, random_obs); 
-collect(modia, :beliefs)
+update!(bu, modia, act, random_obs);   # calculates new belief, and updates within modia
+collect_beliefs(modia)
+
+"""Simulate DCs in the MODIA (does not update belief of modia)"""
+sim = RolloutSimulator(max_steps=5)
+simulate(sim, modia, policies)
+simulate(sim, modia, policies, bu)
+
+"""Simulate DCs in the MODIA, with specified initial states"""
+rng = MersenneTwister()
+s0 = rand(rng, initialstate(modia))   # rng argument is optional
+simulate(sim, modia, policies, bu, s0)
+
+"""History recorder"""
+hr = HistoryRecorder(max_steps=5)
+history = simulate(hr, modia, policies)
+for (i, DPs) in enumerate(history)
+    for (j, DCs) in enumerate(DPs)
+        for (b,a,o,r,t) in eachstep(DCs, "b,a,o,r,t")
+            println("DP: $i, DC: $j -> timestep: $t, reward: $r")
+        end
+    end
+end
+
+
+"""Stepthrough"""
+sthru = stepthrough(modia, policies, bu, "b,a,o,r,t", max_steps=5)
+for (i, DPs) in enumerate(sthru)
+    for (j, DCs) in enumerate(DPs)
+        for (b,a,o,r,t) in DCs
+            println("DP: $i, DC: $j -> timestep: $t, reward: $r")
+        end
+    end
+end
+
+
+"""KMarkovUpdater"""
+k_past_obs = 4
+bu = KMarkovUpdater(modia, k_past_obs)
+rng = MersenneTwister()
+s0 = rand(rng, initialstate(modia))
+initial_observation = rand(rng, initialobs(modia, s0))
+initial_obs_vec = fill(initial_observation, k_past_obs)
+b0 = initialize_belief(bu, initial_obs_vec)
+
+modia = MODIA(DPs, DCs, SSF)
+modia.beliefs = b0
+
+random_obs = [[Bool(rand(0:1)) for _ in sublist] for sublist in modia.markov_prcs]  # create random observations for all DCs
+new_belief = update(bu, modia, act, random_obs);   # calculates new belief, but does not update within modia
